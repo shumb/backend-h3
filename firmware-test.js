af3adc6dff9408768d7725ff1ccdb292e4a259a8
huang <shumb@shumb.cn>
2016-11-10 10:46:17 +0800

    firmware: gd03-ota-signed-full-201611091851.zip

--- firmware-test.js
+++ firmware-test.js
@@ -7,16 +7,23 @@ var util = require('util');
 // http://7xt5k7.com1.z0.glb.clouddn.com/DragonKey_V2.1.rar   md5:e1d2eceb6b2e168e17caf8d1ec74c546
 // url: 'http://7xt5k7.com1.z0.glb.clouddn.com/h3-gd03-rom-beta-2016110311.tar.gz', md5:e7780eb58992fac7c906d54043af98c8
 // http://7xt5k7.com1.z0.glb.clouddn.com/anzhuoshichang_16791539.apk    md5: 4c6554242eb86cc7424001a8f932b164
+// http://7xt5k7.com1.z0.glb.clouddn.com/anzhuoshichang_16791539.apk  4c6554242eb86cc7424001a8f932b164
+var fw_url = 'http://7xt5k7.com1.z0.glb.clouddn.com/gd03-ota-signed-full-201611091851.zip';
+var fw_md5 = 'c0b1f81f9bd93c6370efc75dd5b5cddf';
+var fw_info = '1.update test \n2.settings type changed';
+var fw_ver = '201611091851';
+var fw_type = 1;
+
 var firmware_req = {
 	code: 1,
-	msg: "new udpatezip",
+	msg: "new OTA udpatezip",
 	data: {
 		firmware: {
-			md5: '4c6554242eb86cc7424001a8f932b164',
-			url: 'http://7xt5k7.com1.z0.glb.clouddn.com/anzhuoshichang_16791539.apk',
-			info: '1.new remote-ir supported\n2.test program improved', 
-			version: "2016110311",
-			type: 1,
+			md5: fw_md5,
+			url: fw_url,
+			info: fw_info,
+			version: fw_ver,
+			type: fw_type,
 		},
 		other: "",
 	},
